2c7adea988bdc7d7d19050c4a189ae0ce297eeef
Victor <victoraranguren.dev@gmail.com>
2024-01-30 22:16:03 -0400

    Created and functional basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div className="container p-8 mx-auto">
-      <h1 className="text-3xl font-bold">Bienvenido a IsoAcademy</h1>
+      <div className="relative max-w-3xl px-4 sm:px-6 lg:px-8 mx-auto sm:text-center mt-12">
+        <h1 className="text-center text-6xl font-bold tracking-tight">Bienvenido a IsoAcademy</h1>
+        <p className="my-4 text-lg text-slate-600 dark:text-slate-400">
+          La familia que cuida, aprende y emprende en el metaverso.
+        </p>
+        <button className="p-2 bg-blue-600 rounded text-zinc-50">Comienza a aprender GRATIS</button>
+      </div>
     </div>
   );
 }
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
     <nav className="bg-blue-600 p-6 flex justify-between">
-      <Link to="/" className="text-gray-100 text-xl font-semibold">
+      <Link to="/" className="text-gray-100 text-xl font-semibold mr-4">
         IsoAcademy
       </Link>
-      <div className="inline-flex items-center">
-        <Link to="/cursos" className="text-gray-100">
+      <div className="inline-flex items-center ml-0 mr-auto">
+        <Link to="/cursos" className="text-gray-100 mr-8">
           Cursos
+        </Link>
+        <Link to="/cursos" className="text-gray-100">
+          Talleres
         </Link>
       </div>
       <div className="inline-flex items-center">
